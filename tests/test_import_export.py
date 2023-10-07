b4866e0dd0827cf11a4700f5dbb6a792aa546717
Jesse Vander Does <jvanderdoes@gmail.com>
2023-10-07 11:28:11 -0500

    test: setting up tests

---
+++ tests/test_import_export.py
@@ -0,0 +1,23 @@
+import os
+import slgltf
+
+def setup_module(module):
+    """ setup any state specific to the execution of the given module."""
+    print('setup')
+    if not os.path.exists("glTF-Sample-Models/"):
+        os.system("git init glTF-Sample-Models")
+        os.chdir("glTF-Sample-Models")
+        os.system("git remote add origin https://github.com/KhronosGroup/glTF-Sample-Models.git")
+        os.system("git config core.sparsecheckout true")
+        f = open(".git/info/sparse-checkout", "w+")
+        f.write("2.0/*\n")
+        f.close()
+        os.system("git pull --depth=1 origin master")
+        os.chdir("..")
+
+
+def test_import():
+    gltf = slgltf.slgltf.gltf()
+    gltf.load('./glTF-Sample-Models/2.0/Box/glTF/Box.gltf')
+    data = gltf.data()
+    print(dir(data))
